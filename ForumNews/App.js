e60f10ba4752a3caa4d4e9e04a811270184d695d
King <reyescuadra.valentin@tecnica7.edu.ar>
2025-10-24 17:35:50 -0300

    Al fin esta app de mierda anda y tiene scroll

--- ForumNews/App.js
+++ ForumNews/App.js
@@ -1,82 +1,42 @@
 import React from 'react';
-import { View, Text, Image, Pressable } from 'react-native';
+import { View, Text, Image, Pressable, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import ClimaScreen from './ClimaScreen';
-import DolarScreen from './DolarScreen'
+import DolarScreen from './DolarScreen';
 import ProfileScreen from './ProfileScreen';
 import DeportesScreen from './DeportesScreen';
 import NoticiasScreen from './NoticiasScreen';
 
 function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.header}>¡Javier Milei viaja mañana a Chaco para participar de la convención evangelista!</Text>
       <Image
         source={{ uri:  'https://www.infobae.com/resizer/v2/DK3YCIG77VFA3OMLGTXSJLX6TE.jpg?auth=a794ae30d80c93a2ddb3581f16629fbd2f9128fdbdac58d28089b7d34060caa7&smart=true&width=992&height=661&quality=85' }}
         style={styles.image}
       />
 
-      <Text style={styles.header}>¡La economia corre peligro gracias a la guerra entre Iran e Israel!</Text>
+      <Text style={styles.header}>¡La economía corre peligro gracias a la guerra entre Irán e Israel!</Text>
       <Image
         source={{ uri: 'https://itu.uncuyo.edu.ar/cache/finanzas-18oct_732_1296.png' }}
         style={styles.image}
       />
 
-      <Text style={styles.header}>¡Dragon Ball Super saca el capitulo numero 187 de su Manga!</Text>
+      <Text style={styles.header}>¡Dragon Ball Super saca el capítulo número 187 de su Manga!</Text>
       <Image
         source={{ uri: 'https://sm.ign.com/t/ign_latam/screenshot/default/jcg_rmkw.1280.jpg' }}
         style={styles.image}
       />
-    </View>
-  );
-}
-
-function Profile() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Esta es la pantalla de Perfil</Text>
-    </View>
-  );
-}
-
-function Dolar() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Esta es la pantalla de Dolar</Text>
-    </View>
-  );
-}
-
-function Clima() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Esta es la pantalla de Clima</Text>
-    </View>
-  );
-}
-
-function Noticias() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Esta es la pantalla de Noticias</Text>
-    </View>
-  );
-}
-
-function Deportes() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Esta es la pantalla de Deportes</Text>
-    </View>
+    </ScrollView>
   );
 }
 
 function Entretenimiento() {
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.text}>Esta es la pantalla de Entretenimiento</Text>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -119,7 +79,6 @@ export default function App() {
 
 const styles = {
   container: {
-    flex: 1,
     padding: 20,
     backgroundColor: '#fff',
   },
@@ -133,8 +92,10 @@ const styles = {
     width: '100%',
     height: 180,
     borderRadius: 10,
+    marginBottom: 15,
   },
   text: {
     fontSize: 20,
+    marginBottom: 15,
   },
 };
